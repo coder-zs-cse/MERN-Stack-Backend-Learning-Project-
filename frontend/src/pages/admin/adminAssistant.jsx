cee6d619abf56a66f50d62e43b1e59b627420628
Zubin Shah <www.zubinshah1886@gmail.com>
2024-07-17 15:42:49 +0530

    improved styling of chat sidebar

--- frontend/src/pages/admin/adminAssistant.jsx
+++ frontend/src/pages/admin/adminAssistant.jsx
@@ -29,8 +29,8 @@ const AdminAssistantPage = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row h-screen rounded-md m-2">
-      <div className="w-full md:w-1/3 lg:w-1/4 bg-gray-100 overflow-y-auto rounded-md">
+    <div className="flex flex-col md:flex-row  rounded-md m-2">
+      <div className="w-full md:w-1/3 lg:w-1/4 bg-gray-100 overflow-y-auto rounded-md h-[80vh]">
         <h2 className="text-xl font-semibold p-4 bg-blue-600 text-white">Users</h2>
         <ul className="divide-y divide-gray-200">
           {users.map((user) => (
