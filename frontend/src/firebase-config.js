f419640b7a3775aacb12d55167b186f51ec64fd4
Zubin Shah <www.zubinshah1886@gmail.com>
2024-07-17 15:31:16 +0530

    Chat app integrated

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -2,16 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCzGfBIvn9OrODClkRD4PUv_yvdpxAcw00",
-  authDomain: "onboarding-project-a85c2.firebaseapp.com",
-  databaseURL: "https://onboarding-project-a85c2-default-rtdb.firebaseio.com",
-  projectId: "onboarding-project-a85c2",
-  storageBucket: "onboarding-project-a85c2.appspot.com",
-  messagingSenderId: "1084789892219",
-  appId: "1:1084789892219:web:a0311d532a3910ac136ede",
-  measurementId: "G-JP9TFGY9JX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
